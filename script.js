e31b564f1bc7c0bb706281623f153f80948d16c0
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-08-04 15:04:47 -0500

    2 files modified

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function gquery(data) {
 
   var textBody = JSON.stringify({
     prompt: {
-      text: `put all code in nested pre code tags, ${
+      text: `if the users asks for code, return code blocks, with multiline markdown specifying the codes language, otherwise, behave normally. Question: ${
         document.getElementById('questionBar').value
       }`,
     },
@@ -21,21 +21,52 @@ async function gquery(data) {
   });
 
   const gtext = await response.json();
+  console.log(gtext);
   console.log(gtext.candidates[0].output);
-  chatArray.push(`<p>${gtext.candidates[0].output}</p>`);
-  chatArray.push('output');
-  render();
+  var refined = gtext.candidates[0].output;
+  if (refined) {
+    refined = refined.replace('<', '&lt;');
+    refined = refined.replace('>', '&gt;');
+    refined = refined.replace('/', '&frasl;');
+    refined = refined.replace('```html', '<pre><code class="language-html">');
+    refined = refined.replace(
+      '```javascript',
+      '<pre><code class="language-javascript">'
+    );
+    refined = refined.replace(
+      '```js',
+      '<pre><code class="language-javascript">'
+    );
+    refined = refined.replace('```css', '<pre><code class="language-css">');
+    refined = refined.replace('```', '</pre></code>');
+    chatArray.push(`<p>${refined}</p>`);
+    chatArray.push('output');
+    render();
+  }
 }
 
 function query() {
-  chatArray.push(`<p>${document.getElementById('questionBar').value}</p>`);
+  chatArray.push(`<p>${refineQuery()}</p>`);
   chatArray.push('input');
-  console.log(chatArray);
   gquery();
 }
 
+function refineQuery() {
+  var refined = document.getElementById('questionBar').value;
+  if (refined) {
+    refined = refined.replace('<', '&lt;');
+    refined = refined.replace('>', '&gt;');
+    refined = refined.replace('/', '&frasl;');
+    console.log(refined);
+    return refined;
+  } else {
+    console.log(document.getElementById('questionBar').value);
+  }
+}
+
 function render() {
-  for (let i = 0; i < chatArray.length; i++) {
+  document.getElementById('queries').innerHTML = '';
+  for (let i = 0; i < chatArray.length; i += 2) {
     document.getElementById('queries').innerHTML += chatArray[i];
     hljs.highlightAll();
   }
